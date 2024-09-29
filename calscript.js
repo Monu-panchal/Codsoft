be3d46c6fb89232fb4a323ae49f248c8c3546217
Monu-panchal <themaynk008@gmail.com>
2024-09-29 15:02:38 +0530

    Add files via upload

---
+++ calscript.js
@@ -0,0 +1,26 @@
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;}
+
+
+function clearDisplay() 
+
+{
+    document.getElementById('display').value = '';}
+
+
+function deleteDigit()
+
+{
+    let currentValue = document.getElementById('display').value;
+    document.getElementById('display').value = currentValue.slice(0, -1);}
+
+function calculateResult()
+ {
+    try {
+        let result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
